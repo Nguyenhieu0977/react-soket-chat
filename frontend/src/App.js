d245e3449670ebbf7bcafd08b98ae9e02ce4a1f9
nguyenhieu0977 <nguyenhieu0977@gmial.com>
2021-04-19 11:50:38 +0700

    add auth component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,17 @@ import { Route, Switch } from 'react-router-dom';
 import Home from "./components//Home";
 import Signup from "./components/signup/Signup";
 import Login from "./components/login/Login"
+
+import requireAuth from "./utils/RequireAuth";
+
 import Dashboard from "./components/dashboard/Dashboard"
 import { ToastContainer } from "react-toastify";
 
+
 import axios from "axios";
 axios.defaults.baseURL = "http://127.0.0.1:8000";
+
+
 
 class App extends Component {
   render() {
@@ -19,7 +25,7 @@ class App extends Component {
           <Switch>
             <Route path='/signup' component={Signup} />
             <Route path='/login' component={Login} />
-            <Route path='/dashboard' component={Dashboard} />
+            <Route path="/dashboard" component={requireAuth(Dashboard)} />
             <Route exact path="/" component={Home} />
             <Route path="*">Link not found!</Route>
           </Switch>
